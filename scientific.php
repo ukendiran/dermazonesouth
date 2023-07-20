98710424fdbe9d4586ce1999a9e02f92db432a7d
Ukendiran <ukeendiran@gmail.com>
2023-07-20 10:50:16 +0530

    scientific

--- scientific.php
+++ scientific.php
@@ -2,33 +2,46 @@
 include 'include/navbar.php';
 ?>
 
-    <!-- Page Header Start -->
-    <div class="container-fluid page-header py-1 mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <h1 class="display-5 text-white animated slideInRight">Scientific Program</h1>
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb animated slideInRight mb-0">
-                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                    <!-- <li class="breadcrumb-item"><a href="#">Pages</a></li> -->
-                    <li class="breadcrumb-item active" aria-current="page">Scientific Program</li>
-                </ol>
-            </nav>
-        </div>
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-1 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <h1 class="display-5 text-white animated slideInRight">Scientific Program</h1>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb animated slideInRight mb-0">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <!-- <li class="breadcrumb-item"><a href="#">Pages</a></li> -->
+                <li class="breadcrumb-item active" aria-current="page">Scientific Program</li>
+            </ol>
+        </nav>
     </div>
-    <!-- Page Header End -->
+</div>
+<!-- Page Header End -->
 
 
-    <!-- About Start -->
-    <div class="container-xxl py-1">
-        <div class="container">
-            <div class="row g-5">
-             
-            <div class="col-lg-12 wow fadeIn" data-wow-delay="0.5s">            
-                <h1 class="fw-medium text-uppercase display-5 mb-4 text-center">Coming Soon</h1>
-            </div>              
+<!-- About Start -->
+<div class="container-xxl py-1">
+    <div class="container">
+        <div class="row g-5">
+
+            <div class="col-lg-12 wow fadeIn" data-wow-delay="0.5s">
+                <h1 class="fw-medium text-uppercase display-5 text-start">The scientific program includes</h1>
+            </div>
+
+            <div class="d-flex align-items-center mb-4">
+                <div class="ms-4">
+                    <p><i class="fa fa-check text-primary me-2"></i>Two parallel workshops on 12th October – one on LASERS IN DERMATOLOGY and second on TRICHOLOGY including HAIR TRANSPLANT and SCALP MICROPIGMENTATION</p>
+                    <p><i class="fa fa-check text-primary me-2"></i>CME on PROCEDURAL DERMATOLOGY on 13th October</p>
+                    <p><i class="fa fa-check text-primary me-2"></i>Two parallel scientific sessions focusing on THERAPEUTICS and RECENT ADVANCES on 13th, 14th and 15th</p>
+                    <p><i class="fa fa-check text-primary me-2"></i>Sardari Lal Memorial Oration on 15th</p>
+                    <p><i class="fa fa-check text-primary me-2"></i>Panel discussions on interesting topics</p>
+                    <p><i class="fa fa-check text-primary me-2"></i>Oral Paper & e-poster presentations from selected delegates</p>
+                    <p><i class="fa fa-check text-primary me-2"></i>Quiz program for residents </p>
+
+                </div>
             </div>
         </div>
     </div>
-    <!-- About End -->
-        
-    <?php include 'include/footer.php'; ?>
+</div>
+<!-- About End -->
+
+<?php include 'include/footer.php'; ?>
