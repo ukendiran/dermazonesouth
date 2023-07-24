0471c613b1736bcae7b3869792ade9477884fafe
Ukendiran <ukeendiran@gmail.com>
2023-07-24 10:25:18 +0530

    dashboard

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -11,10 +11,10 @@
                      <a class="nav-link text-white " href="<?php echo $adminurl; ?>">Home</a>
                  </li>
                  <li class="nav-item">
-                     <a class="nav-link text-white" href="<?php echo $baseurl; ?>">Users</a>
+                     <a class="nav-link text-white" href="<?php echo $baseurl; ?>users.php">Users</a>
                  </li>
                  <li class="nav-item">
-                     <a class="nav-link text-white" href="<?php echo $baseurl; ?>users.php">Site</a>
+                     <a class="nav-link text-white" href="<?php echo $baseurl; ?>">Site</a>
                  </li>
                  <!-- Add more navigation items here if needed -->
                  <li class="nav-item dropdown">
